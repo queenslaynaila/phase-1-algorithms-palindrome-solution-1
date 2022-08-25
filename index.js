a9234b58af07bf21e58e1e7d70a094e6194ffcab
Queenslay naila <queenslay.naila@student.moringaschool.com>
2022-08-26 00:47:15 +0300

    Done with assignments

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
-function isPalindrome(word) {
-  // Write your algorithm here
+function reverseString(word) {
+  return word.split("").reverse().join("");
+  return reversedWord;
 }
 
+function isPalindrome(word) {
+  const reversedWord = reverseString(word);
+  return word === reversedWord;
+}
 /* 
   Add your pseudocode here
+  reverse the input string
+
+if the reversed string is the same as the input
+  return true
+else
+  return false
 */
 
 /*
   Add written explanation of your solution here
+  The reverseString is a helper Function.IT
+     takes in a string 
+     splits it to an array
+     reverses its order
+     JOin thhe array so its no longer split
+
+  the IsPalindrom function then takes the word
+    Pases it to the reersestring fuction 
+    places the return in a variable reversedWord
+    uses the strict equality opeartor to compare the word and the reversedWord
 */
 
 // You can run `node index.js` to view these console logs
